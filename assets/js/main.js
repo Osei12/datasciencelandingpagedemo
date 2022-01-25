f70329edd269ecde326380103bbc58885f1f0dc2
osei21 <oseiadu6@gmai.com>
2022-01-25 22:21:17 +0000

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        center: true,
+        nav: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 3,
+            },
+            1000: {
+                items: 3,
+
+            }
+        }
+    })
+})
+
+
+const menu = document.querySelector('.menu')
+const close = document.querySelector('.close')
+const nav = document.querySelector('nav')
+
+menu.addEventListener('click', () => {
+    nav.classList.add('open-nav')
+})
+close.addEventListener('click', () => {
+    nav.classList.remove('open-nav')
+})
+
+
+const toTop = document.querySelector(".to-top-btn");
+const Top = document.getElementById("to-top")
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active")
+    }
+})
+
+Top.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+})
