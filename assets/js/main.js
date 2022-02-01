e33fb14f67c0526c166ca01330a4d252c3b6c68a
osei21 <oseiadu6@gmai.com>
2022-02-01 08:32:47 +0000

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,10 +14,10 @@ $(document).ready(function () {
                 items: 1,
             },
             600: {
-                items: 3,
+                items: 1,
             },
             1000: {
-                items: 3,
+                items: 4,
 
             }
         }
